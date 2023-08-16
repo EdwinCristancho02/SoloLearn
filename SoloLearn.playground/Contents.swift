5021247cf29883354903ea0f6555404c7b7f7d05
EdwinCristancho02 <eyacrimo@gmail.com>
2023-08-15 19:19:19 -0500

    Introduction to  Swift

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,3 +1,11 @@
 import Foundation
 
 print("Hello World")
+var myVariable = 42
+print("The value \(myVariable)")
+// esto es una prueba
+/* this is also a comment,
+ but written over multiple lines */
+
+
+
