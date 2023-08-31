4aec56e044debf0f42a06d56712232026b28d538
EdwinCristancho02 <eyacrimo@gmail.com>
2023-08-30 19:53:52 -0500

    Variables Y Constantes Tutorial MoureDev

--- SoloLearn.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ SoloLearn.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -1,27 +1,44 @@
-import Foundation
-let b = 7
-var a = 42
-print("a is \(a),b is \(b)")
-a = b // a is now equal to 7
-print("a´s value changed to \(a)")
-print(1 + 2) // equals 3
-print(6 - 2) // equals 4
-print(4 * 3) // equals 12
-print(10.0 / 2.5) // equals 4.0
-var a = 4 + 2
-var b = 15 - a
-var result = a / (b - a)
-print (9 % 4)
-var a = 10 , b = 3
-var c = a % (a - b)
-var a = 1
-a += 2
-print(a)
-var a = 1
-a += 2 //a is now equal to 3
-var a = 0
-var b = a+1
-b +=3
-print (b)
+import UIKit
+
+// Variables
+
+/*
+ 
+ bloque de contenidos
+ */
+
+var myFirstVariable = "Hello Hackermen"
+var myFirstNumber = 1
+print(myFirstVariable)
+myFirstVariable = "Bienvenidos a MoureDev"
+print (myFirstVariable)
+//No podemos asignar in tipo entero a una variable de tipo string
+//myFirstVariable = 1
+
+var mySecondVariable = "Hello Hackermen"
+
+print(mySecondVariable)
+
+mySecondVariable = myFirstVariable
+
+print(mySecondVariable)
+
+myFirstVariable = "suscribete"
+
+print(mySecondVariable)
+
+// constantes
+
+let myFirstConstant = "¿Te ha gustado este turorial?"
+
+print(myFirstConstant)
+//una Constante no puede modificar su valor
+//myFirstConstant = "Si te ha gustado, Dale a Like"
+
+let mySecondConstant =  myFirstVariable
+print(myFirstVariable )
+
+
+
 
 
--- SoloLearn.playground/Pages/Variables y Constantes.xcplaygroundpage/Contents.swift
+++ SoloLearn.playground/Pages/Variables y Constantes.xcplaygroundpage/Contents.swift
@@ -1,6 +1,16 @@
-var welcomeMsg: String
-welcomeMsg = "hello"
-print(welcomeMsg)
-let pi = 3.14159 //Double
-print(pi)
+print(1 == 1) // true, because 1 is equal to 1
+print(2 != 1) // true, because 2 is not equal to 1
+print(2 > 1)  // true, because 2 is greater than 1
+print(1 < 2)  // true, because 1 is less than 2
+print(1 >= 1) // true, because 1 is a greater than or equal to 1
+print(2 <= 1) // false, because 2 is not less than or equal to 1
+
+var gender = 1
+gender == 0 ? print("male") : print ("female")
+
+let height = 40
+let isCheck = true
+let rowHeight = height + (isCheck ? 50 : 20)
+
+
 
