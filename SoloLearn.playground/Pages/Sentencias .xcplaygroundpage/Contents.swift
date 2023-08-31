f017d0e5a76c760d98d62ed80397c99888f5d6e7
EdwinCristancho02 <eyacrimo@gmail.com>
2023-08-30 21:43:07 -0500

    Sentencias if else

---
+++ SoloLearn.playground/Pages/Sentencias .xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let myNumber = 12
+
+
+// Operadores Condicionales
+// = = Igualdad
+// > Mayor que
+// < Menor que
+// (> =) Mayor Igual que
+// (< =) Menor Igual que
+// (! =) Desigualdad
+
+if myNumber <  10  {
+    // Sentencias if
+    print("\(myNumber) es menor que 10")
+}  else  {
+    // Sentencia else
+    print("\(myNumber) es mayor o igual que 10")
+}
